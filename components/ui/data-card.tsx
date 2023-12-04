abef428bb3f805255526ff14aadc60c717bc4b68
rosanno <rosannomacabuhay@gmail.com>
2023-12-04 19:33:13 +0800

    update data-card

--- components/ui/data-card.tsx
+++ components/ui/data-card.tsx
@@ -13,7 +13,7 @@ export const DataCard = ({
   data,
 }: DataCardProps) => {
   return (
-    <div className="bg-white rounded-xl p-3 w-full sm:w-[220px]">
+    <div className="relative bg-white rounded-xl p-3 w-full h-full sm:w-[220px]">
       <div className="flex items-center gap-2">
         <div className="bg-gray-400/10 p-2 rounded-full">
           <Icon className="h-4 w-4" />
@@ -22,8 +22,10 @@ export const DataCard = ({
           {label}
         </span>
       </div>
-      <div className="pt-4">
-        <h3 className="text-xl font-semibold">{data}</h3>
+      <div className="py-5">
+        <div className="absolute bottom-3">
+          <h3 className="text-xl font-semibold">{data}</h3>
+        </div>
       </div>
     </div>
   );
