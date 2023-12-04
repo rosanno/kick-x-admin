1529a4a0d2ed22574ea204e108d2996c92c65c52
rosanno <rosannomacabuhay@gmail.com>
2023-12-04 10:00:23 +0800

    update make responsive

--- components/ui/data-card.tsx
+++ components/ui/data-card.tsx
@@ -13,7 +13,7 @@ export const DataCard = ({
   data,
 }: DataCardProps) => {
   return (
-    <div className="bg-white rounded-xl p-3 w-[220px]">
+    <div className="bg-white rounded-xl p-3 w-full sm:w-[220px]">
       <div className="flex items-center gap-2">
         <div className="bg-gray-400/10 p-2 rounded-full">
           <Icon className="h-4 w-4" />
