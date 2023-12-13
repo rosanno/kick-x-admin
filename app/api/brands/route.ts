f23131096e48b420c550efe46b6b531a5642410d
rosanno <rosannomacabuhay@gmail.com>
2023-12-13 10:01:45 +0800

    create post route for brands

--- app/api/brands/route.ts
+++ app/api/brands/route.ts
@@ -19,3 +19,30 @@ export async function GET(req: NextRequest) {
     return NextResponse.json(brands);
   } catch (error) {}
 }
+
+export async function POST(req: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    const { brand_name } = await req.json();
+
+    if (!session?.user?.id) {
+      return new NextResponse("Unauthenticated", {
+        status: 403,
+      });
+    }
+
+    const brand = await prisma.brand.create({
+      data: {
+        brand_name,
+      },
+    });
+
+    return NextResponse.json(brand);
+  } catch (error) {
+    console.log("[BRAND_POST]", error);
+    return new NextResponse("Internal error", {
+      status: 500,
+    });
+  }
+}
