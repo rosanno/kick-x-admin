495a9244376f8189e334c8ecf8235f701ad6a4d7
rosanno <rosannomacabuhay@gmail.com>
2023-12-13 10:04:15 +0800

    update

--- app/(dashboard)/(routes)/brands/page.tsx
+++ app/(dashboard)/(routes)/brands/page.tsx
@@ -2,7 +2,11 @@ import { prisma } from "@/lib/prisma";
 import { Client } from "./_components/client";
 
 const Brands = async () => {
-  const brands = await prisma.brand.findMany();
+  const brands = await prisma.brand.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
   return (
     <div className="bg-white shadow-sm rounded-sm">
