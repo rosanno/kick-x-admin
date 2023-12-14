c1df28606a9c48095a47aacb1eff3e423e38cc86
rosanno <rosannomacabuhay@gmail.com>
2023-12-14 15:27:18 +0800

    update metadata for brands page

--- app/(dashboard)/(routes)/brands/page.tsx
+++ app/(dashboard)/(routes)/brands/page.tsx
@@ -1,6 +1,14 @@
 import { prisma } from "@/lib/prisma";
 import { Client } from "./_components/client";
 
+export const metadata = {
+  title: "Kick X | Brands",
+  openGraph: {
+    title: "Brands",
+    description: "Manage shoe brands",
+  },
+};
+
 const Brands = async () => {
   const brands = await prisma.brand.findMany({
     orderBy: {
