681e1d2913dcb3c4632e3a8828e5f60bd82bae33
rosanno <rosannomacabuhay@gmail.com>
2023-12-17 19:45:06 +0800

    reduce font size

--- app/(dashboard)/(routes)/customers/_components/column.tsx
+++ app/(dashboard)/(routes)/customers/_components/column.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import { format } from "date-fns";
 import { ArrowUpDown } from "lucide-react";
 
 import { CellAction } from "./cell-action";
@@ -18,18 +17,46 @@ export const columns: ColumnDef<CustomerColumn>[] = [
   {
     accessorKey: "customer",
     header: "Customer",
+    cell: ({ row }) => {
+      return (
+        <div className="text-[13px]">
+          {row.getValue("customer")}
+        </div>
+      );
+    },
   },
   {
     accessorKey: "email",
     header: "Email",
+    cell: ({ row }) => {
+      return (
+        <div className="text-[13px]">
+          {row.getValue("email")}
+        </div>
+      );
+    },
   },
   {
     accessorKey: "phone",
     header: "Phone",
+    cell: ({ row }) => {
+      return (
+        <div className="text-[13px]">
+          {row.getValue("phone")}
+        </div>
+      );
+    },
   },
   {
     accessorKey: "created_at",
     header: "Joining Date",
+    cell: ({ row }) => {
+      return (
+        <div className="text-[13px]">
+          {row.getValue("created_at")}
+        </div>
+      );
+    },
   },
   {
     id: "actions",
